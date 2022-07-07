c2e5c75336d618cdb818bfba2039012074439df7
FixArt <79996282+FixArt@users.noreply.github.com>
2022-07-07 21:49:39 +0300

    Fix wrong example

--- Examples/Format example.cpp
+++ Examples/Format example.cpp
@@ -5,20 +5,15 @@ int main(void)
 {
     fileStream<char> a;
     unsigned int b = 5, c = 3;
-    a.open("testable.bin", 2, true);
+    a.open("testable.txt", 2, false);
     if(a.getError() != 0) { a.close(); return a.error; }
-    a.writeBlock(&b, 1);
-    a.writeBlock(&c, 1);
-    //a.writeByFormat("%u|%u", b, c);
+    a.writeByFormat("%u:%u", b, c);
+    if(a.getError() != 0) { a.close(); return a.error; }
     a.close();
     b = 0; c = 0;
-    a.open("testable.bin", 1, true);
-    //a.getByFormat("%u|%u", &b, &c);
-    int *bp = a.readBlock<int>(1);
-    if(a.getError() != 0) { a.close(); return a.error; }
-    int *cp = a.readBlock<int>(1);
+    a.open("testable.txt", 1, false);
+    a.getByFormat("%u:%u", &b, &c);
     if(a.getError() != 0) { a.close(); return a.error; }
     a.close();
-    std::cout << *bp << ":" << *cp << ".\n";
-    
+    std::cout << b << ":" << c << ".\n";
 }
